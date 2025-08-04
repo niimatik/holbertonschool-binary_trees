f23b1dee695109d1acf8f5b01f57950ced2bc66a
niimatik <niimatik.gg@gmail.com>
2025-08-04 15:46:03 +0200

    fix: put into betty format

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -10,12 +10,14 @@
 int is_full(const binary_tree_t *tree)
 {
 	if ((tree->left == NULL && tree->right != NULL) ||
-        (tree->left != NULL && tree->right == NULL))
-        return (0);
-
-    if (is_full(tree->left) == 0 || is_full(tree->right) == 0)
-        return (0);
-
+		(tree->left != NULL && tree->right == NULL))
+	{
+		return (0);
+	}
+	if (is_full(tree->left) == 0 || is_full(tree->right) == 0)
+	{
+		return (0);
+	}
 	return (1);
 }
 
