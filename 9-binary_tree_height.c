7d08ae3d991445d3635bc5c7e08c840f0f80eb86
niimatik <niimatik.gg@gmail.com>
2025-08-04 14:59:02 +0200

    fix: put the file into betty format

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,8 +12,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree)
 	{
 		int l = hauteur(tree->left);
-    	int r = hauteur(tree->right);
-    	return (l > r ? l : r);
+		int r = hauteur(tree->right);
+
+		return (l > r ? l : r);
 	}
 	return (0);
 }
