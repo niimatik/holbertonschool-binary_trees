7cf758ae8c226c77806a03533a928be41ba44d18
niimatik <niimatik.gg@gmail.com>
2025-08-04 11:28:20 +0200

    fix: add condition

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -23,6 +23,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_node->parent = parent;
 	new_node->left = NULL;
 	new_node->right = NULL;
+
+	if (parent->left != NULL)
+		new_node->left = parent->left;
+
 	parent->left = new_node;
 
 	return (new_node);
