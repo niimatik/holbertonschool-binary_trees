c56114a8a0e1f5495a3b5606d58cee8d20dc8670
niimatik <niimatik.gg@gmail.com>
2025-08-04 15:07:15 +0200

    fix: right check

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,7 +12,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree)
 	{
 		int l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-		int r = tree->left ? 1 + binary_tree_height(tree->right) : 0;
+		int r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
 		return ((l > r) ? l : r);
 	}
