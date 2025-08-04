e5d1313d043df88345f99da59e0cfe5c077682be
niimatik <niimatik.gg@gmail.com>
2025-08-04 15:24:41 +0200

    fix: func

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -13,7 +13,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	if (tree)
 	{
-		l += (!tree->left && !tree->right) ? 1 : 0;
+		l += (tree->left || tree->right) ? 1 : 0;
 		l += binary_tree_nodes(tree->left);
 		l += binary_tree_nodes(tree->right);
 	}
