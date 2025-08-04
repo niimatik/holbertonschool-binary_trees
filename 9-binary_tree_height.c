d845ce6c364da2a0942168e492d4d7a37685b410
niimatik <niimatik.gg@gmail.com>
2025-08-04 14:59:52 +0200

    fix: func call

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,8 +11,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree)
 	{
-		int l = hauteur(tree->left);
-		int r = hauteur(tree->right);
+		int l = binary_tree_height(tree->left);
+		int r = binary_tree_height(tree->right);
 
 		return (l > r ? l : r);
 	}
